229f3e50c889b16b9566c581ef4c9659dc8bbdb3
tusharishere <tusharsinha1452@gmail.com>
2024-11-22 15:56:52 +0530

    Application Health

--- comment/src/main/java/com/microservice/comment/controller/CommentController.java
+++ comment/src/main/java/com/microservice/comment/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.microservice.comment.controller;
 
 import com.microservice.comment.entity.Comment;
 import com.microservice.comment.service.CommentService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,7 @@ public class CommentController {
     }
 
     @PostMapping
+    //@CircuitBreaker(name = "commentBreaker", fallbackMethod = "commentFallback")
     public ResponseEntity<Comment> saveComment(@RequestBody Comment comment) {
         Comment savedComment = commentService.saveComment(comment);
         return new ResponseEntity<>(savedComment, HttpStatus.CREATED);
@@ -27,4 +29,5 @@ public class CommentController {
     public List<Comment> getAllCommentsByPostId(@PathVariable String postId){
         return commentService.getCommentByPostId(postId);
     }
+
 }
--- post/src/main/java/com/microservice/post/controller/PostController.java
+++ post/src/main/java/com/microservice/post/controller/PostController.java
@@ -3,6 +3,7 @@ package com.microservice.post.controller;
 import com.microservice.post.entity.Post;
 import com.microservice.post.payload.PostDto;
 import com.microservice.post.service.PostService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -37,9 +38,25 @@ public class PostController {
     }
 
     @GetMapping("/{postId}/comments")
+    @CircuitBreaker(name = "commentBreaker", fallbackMethod = "commentFallback")
     public ResponseEntity<PostDto> getAllCommentsForParticularPost(@PathVariable String postId){
         PostDto postDto =  postService.getAllCommentsForParticularPost(postId);
         return new ResponseEntity<>(postDto, HttpStatus.OK);
+    }
+
+    public ResponseEntity<PostDto> commentFallback(String postId, Exception ex) {
+
+        System.out.println("Fallback is executed because service is down : "+ ex.getMessage());
+
+        // ex.printStackTrace();
+
+        PostDto dto = new PostDto();
+        dto.setId("1234");
+        dto.setTitle("Service Down");
+        dto.setContent("Service Down");
+        dto.setDescription("Service Down");
+        //dto.setComments(Arrays.("Service Down"));
+        return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
     }
 
 }
